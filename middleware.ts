0a6f096536cf1af4352240c23914e2c6bed8c4cf
Rj45-143 <arjaygaralde45@gmail.com>
2026-01-15 12:40:29 +0800

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const PUBLIC_PATHS = ["/login", "/api/login"];
+const PUBLIC_PATHS = ["/","/login", "/api/login"];
 
 export function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
